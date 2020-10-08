7b12070f42e58bca8b9455748f73934cc95e31a5
Lucas Lopes <lucasclopesr@gmail.com>
2020-10-08 13:34:18 -0300

    Fix quotes in ldap_change_password

--- scripts/change_ldap_password_example.sh
+++ scripts/change_ldap_password_example.sh
@@ -1,1 +1,1 @@
-ldappasswd -H ldap://192.168.0.3 -x -D 'uid=$USER,ou=People,dc=example,dc=com' -W -A -S
+ldappasswd -H ldap://192.168.0.3 -x -D "uid=$USER,ou=People,dc=example,dc=com" -W -A -S
--- scripts/generate_password_change.sh
+++ scripts/generate_password_change.sh
@@ -7,6 +7,6 @@ if [ -z "$1" ]
     then
       echo "You must provide the organization DN as second argument"
   else
-    echo "ldappasswd -H ldap://${1} -x -D 'uid=\$USER,${2}' -W -A -S" >> change_ldap_password.sh
+    echo "ldappasswd -H ldap://${1} -x -D \"uid=\$USER,${2}\" -W -A -S" >> change_ldap_password.sh
     chmod +x change_ldap_password.sh
 fi
